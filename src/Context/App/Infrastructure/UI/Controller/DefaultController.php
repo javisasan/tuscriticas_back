d763540d474132a2a4e4734d90b73e4f757511f8
Javier Sagra <javisasan@gmail.com>
2022-10-22 12:48:05 +0200

    MR-1_create_basic_structure

---
+++ src/Context/App/Infrastructure/UI/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CommonPlatform\Context\App\Infrastructure\UI\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    public function index(): Response
+    {
+        return new JsonResponse(['status' => 'ok', 'asdf' => 'fdsfsdfsdf']);
+    }
+}
+
